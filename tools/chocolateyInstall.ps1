2fd38182103b99f1ca59b459ffa4e5cf6bfc4e7b
angryhussord <angryhussord@outlook.com>
2015-07-31 15:58:31 -0700

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'hashtab' 
+$installerType = 'exe' 
+$url = 'http://downloads.implbits.com/v1/content/4Jg3JKtKbCKUgGYDSScHD4/HashTab_v5.2.0.14_Setup.exe' 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
