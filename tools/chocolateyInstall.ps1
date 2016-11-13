499b180e90cdb116beabd68f7606c1f9685a405b
Patrick Hufford <pahuff@microsoft.com>
2016-11-13 05:16:28 -0800

    updated for 6.0.0.28

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,20 @@
-﻿$packageName = 'hashtab' 
-$installerType = 'exe' 
-$url = 'http://implbits.com/bl/chocolatey/non-commercial/HashTab_v5.2.0.14_Setup.exe' 
-$silentArgs = '/S' 
-$validExitCodes = @(0) 
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageName = 'hashtab';
+$installerType = 'exe';
+$url = 'http://implbits.com/products/hashtab/HashTab_v6.0.0.28_Setup.exe';
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+  softwareName  = 'hashtab'
+  checksum      = '472420807787F665BD36C07D45E51374DFF5D4215C568DD05E5672D56196F48A'
+  checksumType  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;
